580c79dda8656730f456148e08794874a2bf3fe5
Sandoval Güicho <sandovalguichoo@gmail.com>
2022-12-06 19:06:03 -0600

    inicializando-y-cargando-proyecto-ok

---
+++ src/main/java/com/spring/cloud/config/server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+	
+	/* Configuración:
+	 * - Es servidor Spring Cloud Config
+	 */
+
+}
