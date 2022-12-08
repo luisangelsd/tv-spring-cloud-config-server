b7bff7912169196aeb760f33844c09e6defbbf0e
Sandoval Güicho <sandovalguichoo@gmail.com>
2022-12-08 17:41:24 -0600

    configurado-para-heroku

---
+++ src/main/java/com/spring/cloud/config/server/ServletInitializer.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.config.server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/* Configuración de arranque para plataformas externas como Heroku */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringCloudConfigServerApplication.class);
+	}
+	
+	
+
+}
